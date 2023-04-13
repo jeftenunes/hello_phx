a0d68e5c4bf04c74dd7a4d96e626d3d0fd91be4b
jeftenunes <jeftenunes@hotmail.com>
2023-04-13 19:50:57 -0400

    estudos

--- lib/hello_phx_web/controllers/hello_controller.ex
+++ lib/hello_phx_web/controllers/hello_controller.ex
@@ -1,7 +1,23 @@
 defmodule HelloPhxWeb.HelloController do
   use HelloPhxWeb, :controller
+  @greets [%{"id" => 1, name: "jefte"}, %{"id" => 2, name: "joao"}]
 
   def index(conn, _params) do
-    render(conn, :index)
+    render(conn, users: @greets)
+  end
+
+  def show(conn, %{"id" => id}) do
+    render(conn, user: %{"id" => 1, name: "jefte"})
+  end
+
+  def update(conn, %{"id" => id, name: name}) do
+    render(conn, user: %{"id" => id, "name" => name})
+  end
+
+  def create(conn, %{"id" => id, name: name}) do
+    render(conn, 201, user: %{"id" => id, "name" => name})
+  end
+
+  def delete(conn, %{"id" => id}) do
   end
 end
